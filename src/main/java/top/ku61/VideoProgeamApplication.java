0d972a37c5baf896c566d5911e183dd06ce2233b
ku61 <2055678692@qq.com>
2021-07-21 15:34:36 +0800

    初始化-创建项目

---
+++ src/main/java/top/ku61/VideoProgeamApplication.java
@@ -0,0 +1,13 @@
+package top.ku61;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoProgeamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoProgeamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/ku61/VideoProgeamApplicationTests.java
@@ -0,0 +1,13 @@
+package top.ku61;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoProgeamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
